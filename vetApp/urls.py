a0a23773599caa98ff68cc22f76b23b7e2a8ea20
fernando <86822448+fernando-fate@users.noreply.github.com>
2025-07-05 08:55:09 -0600

    Pude unir tanto el landing page con el login y el register y con la pagina de citas

--- vetApp/urls.py
+++ vetApp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns = [
-  path('', views.home, name='home'),
-  path('', views.citas, name='citas'),
   # path('login/', views.login_view, name='login'),
   # path('logout/', views.logout_view, name='logout'),
   # path('register/', views.register, name='register'),
+
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('citas/', views.citas, name='citas'),
+  path('register/', views.register, name='register'),
+  path('login/', views.login_view, name='login')
+
 
 ]
--- vetApp/views.py
+++ vetApp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta, time
+
+from django.shortcuts import redirect, render
+
+from vetApp.forms import LoginForm
 
 # Create your views here.
 
@@ -13,3 +17,31 @@ def citas(request):
     contexto = {}
     
     return render(request, 'Home/GestionCitas.html', contexto)
+
+
+def register(request):
+    """Vista para registro de usuarios"""
+    context = {
+        'data': {},
+        'errors': {},
+        'especialidades':["dopakdoka", 'odaokdo'], #Doctor.ESPECIALIDADES,
+        'yesterday': datetime.now().date() - timedelta(days=1),
+        # 'debug': settings.DEBUG
+    }
+      
+    return render(request, 'register/register.html', context)
+  
+  
+def login_view(request):
+
+  if request.method == 'POST':
+    return redirect('citas')
+  else:
+    form = LoginForm()
+  response = render(request, 'login/login.html', {
+        'form': form,
+        # 'debug': settings.DEBUG
+    })
+  
+  
+  return response
